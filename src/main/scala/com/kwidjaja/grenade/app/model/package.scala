d221e5ed222df0f901868316c676ac5533944b1d
Kristian Widjaja <kwidjaja1312@gmail.com>
2019-10-03 19:13:19 +0800

    Initiate type of models

---
+++ src/main/scala/com/kwidjaja/grenade/app/model/package.scala
@@ -0,0 +1,13 @@
+package com.kwidjaja.grenade.app
+
+/**
+  * Common/Generic classes
+  *
+  * @author widjajk
+  * @since 3/10/19 7:10 PM
+  */
+package object model {
+
+  case class Point(x: Int, y: Int)
+  case class Radius(width: Int)
+}
