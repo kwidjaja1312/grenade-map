a673bbe54726a4f2b0ab9a28ee44ee370290cdfc
Kristian Widjaja <kwidjaja1312@gmail.com>
2019-10-05 15:32:41 +0800

    Added akka related dependencies and remove some scalac options

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,8 +9,16 @@ import sbt._
 object Dependencies {
 
   lazy val scalaTestVersion = "3.0.8"
+  lazy val typesafeConfig = "1.3.4"
+  lazy val akkaHttp = "10.1.10"
+  lazy val akka = "2.5.25"
 
   val dependencies: Seq[ModuleID] = Seq(
-    "org.scalatest" % "scalatest_2.13" % scalaTestVersion % "test"
+    "com.typesafe.akka" %% "akka-http"            % akkaHttp,
+    "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttp,
+    "com.typesafe.akka" %% "akka-stream"          % akka,
+    "org.scalatest" %% "scalatest"                % scalaTestVersion % "test"
+  ) ++ Seq(
+    "com.typesafe" % "config" % typesafeConfig
   )
 }
